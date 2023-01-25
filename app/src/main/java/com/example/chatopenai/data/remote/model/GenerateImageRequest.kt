7672338436b63103e36c424944e5576d46610fbc
Pedro Henrique <pedro.steam2016@hotmail.com>
2023-01-25 09:14:58 -0300

    Completing the generateImagesRequest model

--- app/src/main/java/com/example/chatopenai/data/remote/model/GenerateImageRequest.kt
+++ app/src/main/java/com/example/chatopenai/data/remote/model/GenerateImageRequest.kt
@@ -1,5 +1,10 @@
 package com.example.chatopenai.data.remote.model
 
+import com.google.gson.annotations.SerializedName
+
 data class GenerateImageRequest(
-    val prompt: String
+    val prompt: String,
+    @SerializedName("n")
+    val numberOfGenerateImages: Int = 1,
+    val size: String = "1024x1024"
 )
